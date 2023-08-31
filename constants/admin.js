a8666c83b07e6a1a90ede74d48a9ed110cd8814c
Rontu Barhoi <46859706+Rontu22@users.noreply.github.com>
2023-09-01 00:31:42 +0530

    Added message, total in each bulk get api call

---
+++ constants/admin.js
@@ -0,0 +1,10 @@
+const DEFAULT_GROUP_ID = 1;
+const DEFAULT_GROUP_NAME = "General";
+const DEFAULT_GROUP_IMAGE =
+  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+
+module.exports = {
+  DEFAULT_GROUP_ID,
+  DEFAULT_GROUP_NAME,
+  DEFAULT_GROUP_IMAGE,
+};
